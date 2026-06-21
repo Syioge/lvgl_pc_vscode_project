cdffd80d5e2d468cf33122a41d1a7032445ae72b
Syioge <2422614852@qq.com>
2026-06-21 18:36:57 +0800

    示例2:按键的样式

--- src/main.c
+++ src/main.c
@@ -52,17 +52,40 @@
 
 #if LV_USE_OS != LV_OS_FREERTOS
 
-void btn_event_cb(lv_event_t * e)
-{
-  lv_event_code_t code = lv_event_get_code(e);
-  lv_obj_t * btn = lv_event_get_target_obj(e);
-  if(code == LV_EVENT_CLICKED) {
-    static uint32_t count = 0;
-    count++;
+lv_style_t style_btn;
+lv_style_t style_button_pressed;
+lv_style_t style_button_red;
 
-    lv_obj_t * label = lv_obj_get_child(btn, 0);
-    lv_label_set_text_fmt(label, "Button: %d", count);
-  }
+lv_color_t darken(const lv_color_filter_dsc_t *dsc, lv_color_t color, lv_opa_t opa)
+{
+  LV_UNUSED(dsc);
+  return lv_color_darken(color, opa);
+}
+
+void style_init(void)
+{
+  lv_style_init(&style_btn);
+  lv_style_set_radius(&style_btn, 10);
+  lv_style_set_bg_opa(&style_btn, LV_OPA_COVER);
+  lv_style_set_bg_color(&style_btn, lv_palette_lighten(LV_PALETTE_GREY, 3));
+  lv_style_set_bg_grad_color(&style_btn, lv_palette_main(LV_PALETTE_GREY));
+  lv_style_set_bg_grad_dir(&style_btn, LV_GRAD_DIR_VER);
+
+  lv_style_set_border_color(&style_btn, lv_color_black());
+  lv_style_set_border_opa(&style_btn, LV_OPA_20);
+  lv_style_set_border_width(&style_btn, 2);
+  lv_style_set_text_color(&style_btn, lv_color_black());
+  /*Create a style for the pressed state.
+    *Use a color filter to simply modify all colors in this state*/
+  static lv_color_filter_dsc_t color_filter;
+  lv_color_filter_dsc_init(&color_filter, darken);
+  lv_style_init(&style_button_pressed);
+  lv_style_set_color_filter_dsc(&style_button_pressed, &color_filter);
+  lv_style_set_color_filter_opa(&style_button_pressed, LV_OPA_20);
+  /*Create a red style. Change only some colors.*/
+  lv_style_init(&style_button_red);
+  lv_style_set_bg_color(&style_button_red, lv_palette_main(LV_PALETTE_RED));
+  lv_style_set_bg_grad_color(&style_button_red, lv_palette_lighten(LV_PALETTE_RED, 3));
 }
 
 int main(int argc, char **argv)
@@ -76,22 +99,34 @@ int main(int argc, char **argv)
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
   sdl_hal_init(320, 240);
 
-  /* Run the default demo */
-  /* To try a different demo or example, replace this with one of: */
-  /* - lv_demo_benchmark(); */
-  /* - lv_demo_stress(); */
-  /* - lv_example_label_1(); */
-  /* - etc. */
-  // lv_demo_widgets();
-
-  lv_obj_t * btn = lv_button_create(lv_screen_active());
-  lv_obj_set_pos(btn, 10, 10);
-  lv_obj_set_size(btn, 120, 50);
-  lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);
-
-  lv_obj_t * label = lv_label_create(btn);
-  lv_label_set_text(label, "Button");
-  lv_obj_center(label);
+  /*Initialize the style*/
+    style_init();
+    /*Create a button and use the new styles*/
+    lv_obj_t * btn = lv_button_create(lv_screen_active());
+    /* Remove the styles coming from the theme
+     * Note that size and position are also stored as style properties
+     * so lv_obj_remove_style_all will remove the set size and position too */
+    lv_obj_remove_style_all(btn);
+    lv_obj_set_pos(btn, 10, 10);
+    lv_obj_set_size(btn, 120, 50);
+    lv_obj_add_style(btn, &style_btn, 0);
+    lv_obj_add_style(btn, &style_button_pressed, LV_STATE_PRESSED);
+    /*Add a label to the button*/
+    lv_obj_t * label = lv_label_create(btn);
+    lv_label_set_text(label, "Button");
+    lv_obj_center(label);
+    /*Create another button and use the red style too*/
+    lv_obj_t * btn2 = lv_button_create(lv_screen_active());
+    lv_obj_remove_style_all(btn2);                      /*Remove the styles coming from the theme*/
+    lv_obj_set_pos(btn2, 10, 80);
+    lv_obj_set_size(btn2, 120, 50);
+    lv_obj_add_style(btn2, &style_btn, 0);
+    lv_obj_add_style(btn2, &style_button_red, 0);
+    lv_obj_add_style(btn2, &style_button_pressed, LV_STATE_PRESSED);
+    lv_obj_set_style_radius(btn2, LV_RADIUS_CIRCLE, 0); /*Add a local style too*/
+    label = lv_label_create(btn2);
+    lv_label_set_text(label, "Button 2");
+    lv_obj_center(label);
 
   while(1) {
     /* oeriodically call the lv_task handler.
