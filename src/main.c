192e450069353d9de783a954b26c77c69cdf07d0
Syioge <2422614852@qq.com>
2026-06-21 18:18:32 +0800

    示例1: 基础按键和事件触发

--- src/main.c
+++ src/main.c
@@ -52,6 +52,19 @@
 
 #if LV_USE_OS != LV_OS_FREERTOS
 
+void btn_event_cb(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t * btn = lv_event_get_target_obj(e);
+  if(code == LV_EVENT_CLICKED) {
+    static uint32_t count = 0;
+    count++;
+
+    lv_obj_t * label = lv_obj_get_child(btn, 0);
+    lv_label_set_text_fmt(label, "Button: %d", count);
+  }
+}
+
 int main(int argc, char **argv)
 {
   (void)argc; /*Unused*/
@@ -61,7 +74,7 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  sdl_hal_init(320, 480);
+  sdl_hal_init(320, 240);
 
   /* Run the default demo */
   /* To try a different demo or example, replace this with one of: */
@@ -69,10 +82,19 @@ int main(int argc, char **argv)
   /* - lv_demo_stress(); */
   /* - lv_example_label_1(); */
   /* - etc. */
-  lv_demo_widgets();
+  // lv_demo_widgets();
+
+  lv_obj_t * btn = lv_button_create(lv_screen_active());
+  lv_obj_set_pos(btn, 10, 10);
+  lv_obj_set_size(btn, 120, 50);
+  lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);
+
+  lv_obj_t * label = lv_label_create(btn);
+  lv_label_set_text(label, "Button");
+  lv_obj_center(label);
 
   while(1) {
-    /* Periodically call the lv_task handler.
+    /* oeriodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
     uint32_t sleep_time_ms = lv_timer_handler();
     if(sleep_time_ms == LV_NO_TIMER_READY){
@@ -94,4 +116,3 @@ int main(int argc, char **argv)
 /**********************
  *   STATIC FUNCTIONS
  **********************/
-
